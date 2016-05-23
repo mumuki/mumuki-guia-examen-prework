f5a814dfd13267ff30fe118db8f825a1ccafb344
franco@mumuki.org <franco@mumuki.org>
2016-05-23 16:53:54 -0400

    Mumuki Export on 2016-05-23 16:53:54 -0400

--- 00004_Profesora furiosa/test.js
+++ 00004_Profesora furiosa/test.js
@@ -1,23 +1,23 @@
 describe("", () => {
   it("seAbre([10, -5, 3, 0], 4)", () => {
-    assert(seAbre([10, -5, 3, 0], 4), false)
+    assert(!seAbre([10, -5, 3, 0], 4))
   })
   
   it("seAbre([10, -5, 3, 0], 3)", () => {
-    assert(seAbre([10, -5, 3, 0], 3), false)
+    assert(!seAbre([10, -5, 3, 0], 3))
   })
   
   it("seAbre([10, -5, 3, 0], 2)", () => {
-    assert(seAbre([10, -5, 3, 0], 2), true)
+    assert(seAbre([10, -5, 3, 0], 2))
   })
   
   it("seAbre([10, -5, 3, 0], 1)", () => {
-    assert(seAbre([10, -5, 3, 0], 1), true)
+    assert(seAbre([10, -5, 3, 0], 1))
   })
   
     
   it("seAbre([0, 0, 3, 0], 3)", () => {
-    assert(seAbre([0, 0, 3, 0], 3), true)
+    assert(seAbre([0, 0, 3, 0], 3))
   })
   
 })
