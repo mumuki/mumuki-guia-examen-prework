bf523f417126d1ab3b2931d62e0d9d1cd83525f3
franco@mumuki.org <franco@mumuki.org>
2016-05-23 13:34:28 -0400

    Mumuki Export on 2016-05-23 13:34:28 -0400

--- 00001_Productoria/test.js
+++ 00001_Productoria/test.js
@@ -1,21 +1,21 @@
 describe("", () => {
   it("productoria([1, 2, 7]) es 14", () => {
-    assert.equal([1, 2, 7], 14);
+    assert.equal(productoria([1, 2, 7]), 14);
   })
   
   it("productoria([10, 10]) es 100", () => {
-    assert.equal([10, 10], 100);
+    assert.equal(productoria([10, 10]), 100);
   })
   
   it("productoria([10]) es 10", () => {
-    assert.equal([10], 10);
+    assert.equal(productoria([10]), 10);
   })
   
   it("productoria([]) es 1", () => {
-    assert.equal([], 1);
+    assert.equal(productoria([]), 1);
   })
   
   it("productoria([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) es 10", () => {
-    assert.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3628800);
+    assert.equal(productoria([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 3628800);
   })
 })
