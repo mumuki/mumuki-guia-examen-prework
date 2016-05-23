efc0a3ac93459c93dc17b6d6773fef2db7cc2022
franco@mumuki.org <franco@mumuki.org>
2016-05-23 15:10:30 -0400

    Mumuki Export on 2016-05-23 15:10:30 -0400

---
+++ 00006_Factorial/description.md
@@ -0,0 +1,2 @@
+Escribí una función `factorial`, que calcule el [factorial](https://es.wikipedia.org/wiki/Factorial) de un numero.
+
---
+++ 00006_Factorial/test.js
@@ -0,0 +1,21 @@
+describe("", () => {
+  it("factorial(1)", () => {
+    assert.equal(factorial(1), 1);
+  })
+  it("factorial(2)", () => {
+    assert.equal(factorial(2), 2);
+  })
+  it("factorial(3)", () => {
+    assert.equal(factorial(3), 6);
+  })
+  it("factorial(1)", () => {
+    assert.equal(factorial(4), 12);
+  })
+  it("factorial(1)", () => {
+    assert.equal(factorial(5), 60);
+  })
+  it("factorial(1)", () => {
+    assert.equal(factorial(6), 360);
+  })
+  
+})
