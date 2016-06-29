40b00756e9300313f5028c17d19f356114773145
franco@mumuki.org <franco@mumuki.org>
2016-06-29 08:47:07 -0400

    Mumuki Export on 2016-06-29 08:47:07 -0400

--- 00004_Profesora furiosa/test.js
+++ 00004_Profesora furiosa/test.js
@@ -1,28 +1,28 @@
 describe("", () => {
   it("seAbre([10, -5, 3, 0], 4)", () => {
-    assert(!seAbre([10, -5, 3, 0], 4))
+    assert.equal(!seAbre([10, -5, 3, 0], 4), true)
   })
   
   it("seAbre([10, -5, 3, 0], 3)", () => {
-    assert(!seAbre([10, -5, 3, 0], 3))
+    assert.equal(!seAbre([10, -5, 3, 0], 3), true)
   })
   
   it("seAbre([10, -5, 3, 0], 2)", () => {
-    assert(seAbre([10, -5, 3, 0], 2))
+    assert.equal(seAbre([10, -5, 3, 0], 2), true)
   })
   
   it("seAbre([10, -5, 3, 0], 1)", () => {
-    assert(seAbre([10, -5, 3, 0], 1))
+    assert.equal(seAbre([10, -5, 3, 0], 1), true)
   })
   
     
   it("seAbre([0, 0, 3, 0], 3)", () => {
-    assert(seAbre([0, 0, 3, 0], 3))
+    assert.equal(seAbre([0, 0, 3, 0], 3), true)
   })
   
   
-  it("aperturas([[0, 0, 3, 0], [1, 2, 4, 5], [0, 0, -1], 3)", () => {
-    assert(aperturas([[0, 0, 3, 0], [1, 2, 4, 5], [0, 0, -1]], 3), [true, false, false])
+  it("aperturas([[0, 0, 3, 0], [1, 2, 4, 5], [0, 0, -1]], 3)", () => {
+    assert.equal(aperturas([[0, 0, 3, 0], [1, 2, 4, 5], [0, 0, -1]], 3), [true, false, false])
   })
   
 })
