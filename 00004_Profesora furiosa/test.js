00bfd5b2710cf406897f51c608f5630a06de02bb
franco@mumuki.org <franco@mumuki.org>
2016-06-29 08:49:45 -0400

    Mumuki Export on 2016-06-29 08:49:45 -0400

--- 00004_Profesora furiosa/test.js
+++ 00004_Profesora furiosa/test.js
@@ -1,23 +1,23 @@
 describe("", () => {
   it("seAbre([10, -5, 3, 0], 4)", () => {
-    assert.equal(!seAbre([10, -5, 3, 0], 4), true)
+    assert.equal(seAbre([10, -5, 3, 0], 4), false, "debería devolver false")
   })
   
   it("seAbre([10, -5, 3, 0], 3)", () => {
-    assert.equal(!seAbre([10, -5, 3, 0], 3), true)
+    assert.equal(seAbre([10, -5, 3, 0], 3), false, "debería devolver false")
   })
   
   it("seAbre([10, -5, 3, 0], 2)", () => {
-    assert.equal(seAbre([10, -5, 3, 0], 2), true)
+    assert.equal(seAbre([10, -5, 3, 0], 2), true, "debería devolver true")
   })
   
   it("seAbre([10, -5, 3, 0], 1)", () => {
-    assert.equal(seAbre([10, -5, 3, 0], 1), true)
+    assert.equal(seAbre([10, -5, 3, 0], 1), true, "debería devolver true")
   })
   
     
   it("seAbre([0, 0, 3, 0], 3)", () => {
-    assert.equal(seAbre([0, 0, 3, 0], 3), true)
+    assert.equal(seAbre([0, 0, 3, 0], 3), true, "debería devolver true")
   })
   
   
